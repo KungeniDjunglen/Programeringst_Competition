0aa4ad5949ae6686d86e80e3cfec20acb47a9d2f
Admin <Admin@TEB127010>
2022-11-25 08:51:09 +0100

    Add project files.

---
+++ Carots/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Carrot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int carrots = 40;
+            Console.Write("Tors tid ? ");
+            int Ttid = int.Parse(Console.ReadLine());
+            Console.Write("Mors tid ? ");
+            int Mtid = int.Parse(Console.ReadLine());
+            carrots = carrots - 2;
+            int Tcarrots = 1;
+            int Mcarrots = 1;
+            for (int i = 0; carrots >= 1; i++)
+            {
+                if (carrots == 1 && i % Ttid == 0 && i % Mtid == 0)
+                {
+                    break;
+                }
+                if(i % Ttid == 0)
+                {
+                    carrots--;
+                    Tcarrots++;
+                }
+                if (i % Mtid == 0)
+                {
+                    carrots--;
+                    Mcarrots++;
+                }
+                Console.WriteLine(carrots);
+            }
+            Console.WriteLine("Programmet är färdigt");
+            Console.WriteLine("Tor fick: " + Tcarrots + ". Mor fick: " + Mcarrots);
+        }
+    }
+}
